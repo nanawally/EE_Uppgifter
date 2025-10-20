370d3b007c2996db7ddc22668b26c5e864f82782
Anna Wallström <anoli89@hotmail.com>
2025-10-20 15:16:21 +0200

    Lektion 4 uppgifter setup

---
+++ lektion-4-uppgifter/src/main/java/com/nanawally/lektion_4_uppgifter/Lektion4UppgifterApplication.java
@@ -0,0 +1,13 @@
+package com.nanawally.lektion_4_uppgifter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lektion4UppgifterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lektion4UppgifterApplication.class, args);
+	}
+
+}
---
+++ lektion-4-uppgifter/src/test/java/com/nanawally/lektion_4_uppgifter/Lektion4UppgifterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanawally.lektion_4_uppgifter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lektion4UppgifterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
